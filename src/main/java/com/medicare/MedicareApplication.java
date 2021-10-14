14600e439b5bbe284b67f0c228966c84ec1ef312
sahil.jayswal <sahil.jayswal1995@gmail.com>
2021-10-14 14:27:49 +0530

    AWS cloud deployment

--- src/main/java/com/medicare/MedicareApplication.java
+++ src/main/java/com/medicare/MedicareApplication.java
@@ -1,17 +1,25 @@
 package com.medicare;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.medicare.model.Product;
+import com.medicare.model.User;
+import com.medicare.repository.UserRepository;
+import com.medicare.service.ProductService;
+
 
 @SpringBootApplication
-public class MedicareApplication {
+public class MedicareApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicareApplication.class, args);
 	}
 
+	
 	/*
 	 * @Autowired UserRepository userRepo;
 	 * 
@@ -40,5 +48,6 @@ public class MedicareApplication {
 	 * 
 	 * }
 	 */
+	 
 
 }
